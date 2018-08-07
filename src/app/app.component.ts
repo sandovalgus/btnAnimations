76429c42c90610cd03814e46a34d35b535263199
gus <gustavo.fabian.sandoval@gmail.com>
2018-08-07 13:25:41 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import * as btnVoice from '../assets/js/btnVoice.js';
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+voiceStatus:number; // 0= sin accion, 1= comienza a grabar, 2= pause | btn opciones
+className: string = 'one-class';
   constructor(public navCtrl: NavController) {
-
+    this.voiceStatus=0;
   }
 
+  btnVoice(){
+    console.log("touch !!");
+    this.className = 'one-class';
+    this.voiceStatus = 2;
+    
+  }
+
+  btnVerVoice(){
+    this.className = 'another-class';
+  }
+
+  goPage(){
+    this.navCtrl.push("Page2Page");
+  }
 }
+ 
